f18712cf380a751a0ccfed507d69415ae9f6d29c
aardperera <randiniama1@gmail.com>
2023-03-14 19:50:09 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -39,4 +39,5 @@ else:
     print("Incorrect!")
 
 
-print("You got "+ str(score) + "questions correct!")
+print("You got "+ str(score) + " questions correct!")
+print("You got "+ str((score/4)*100) +" %.")
