118c48a93b2494d3246fbd30a4948a3204aaac22
aardperera <randiniama1@gmail.com>
2023-03-14 18:57:34 +0530

    Update quiz.py
    create questions

--- quiz.py
+++ quiz.py
@@ -2,3 +2,34 @@ print("Welcome to the Quiz!")
 
 playing = input("Do you want to play? ")
 print(playing)
+
+if playing != "yes":
+    quit()
+
+print("Okay Let's play :) ")
+
+answer = input("What does CPU stand for? ")
+if answer == "Central Processing Unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does USB stand for? ")
+if answer == "Universal Serial Bus":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "Random Access Memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does ATM stand for? ")
+if answer == "Automated Teller Machine":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+
