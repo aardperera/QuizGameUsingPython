27c61cb80805f8df540bf1bbd0c40fbb7978f8e9
aardperera <randiniama1@gmail.com>
2023-03-14 19:07:09 +0530

    Update quiz.py
    answers getting in lowercase

--- quiz.py
+++ quiz.py
@@ -3,31 +3,31 @@ print("Welcome to the Quiz!")
 playing = input("Do you want to play? ")
 print(playing)
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay Let's play :) ")
 
 answer = input("What does CPU stand for? ")
-if answer == "Central Processing Unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
 
 answer = input("What does USB stand for? ")
-if answer == "Universal Serial Bus":
+if answer.lower() == "universal serial bus":
     print("Correct!")
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
-if answer == "Random Access Memory":
+if answer.lower() == "random access memory":
     print("Correct!")
 else:
     print("Incorrect!")
 
 answer = input("What does ATM stand for? ")
-if answer == "Automated Teller Machine":
+if answer.lower() == "automated teller machine":
     print("Correct!")
 else:
     print("Incorrect!")
